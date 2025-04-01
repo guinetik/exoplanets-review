33176df3c9563d723d49c3c52d02f0fe6c7118c0
guinetik <guinetik@gmail.com>
2025-04-01 00:29:17 -0300

    small layout adjustments

--- src/components/bg.jsx
+++ src/components/bg.jsx
@@ -1,7 +1,7 @@
 /* Snatched from: https://codepen.io/ungkunazmi/pen/MWeqVjG */
 export default function StarsBg() {
   return (
-    <div className=" overflow-hidden pointer-events-none fixed top-0 left-0 bottom-0 right-0">
+    <div className="bg overflow-hidden pointer-events-none fixed top-0 left-0 bottom-0 right-0">
       <div className="stars"></div>
       <div className="stars2"></div>
       <div className="stars3"></div>
